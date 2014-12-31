26fadee821a26191b6468c3e843c0f8de0eb0981
json <492103904@qq.com>
2014-12-31 20:01:14 +0800

    项目

---
+++ ThinkPHP3.2/Library/Vendor/Hprose/HproseIO.php
@@ -0,0 +1,27 @@
+<?php
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.net/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+
+/**********************************************************\
+ *                                                        *
+ * HproseIO.php                                           *
+ *                                                        *
+ * hprose io library for php5.                            *
+ *                                                        *
+ * LastModified: Nov 10, 2013                             *
+ * Author: Ma Bingyao <andot@hprfc.com>                   *
+ *                                                        *
+\**********************************************************/
+
+require_once('../ThinkPHP/Library/Vendor/Hprose../../../../Library/HproseClassManager.phpproseTags.ph../ThinkPHP/Library/Vendor/Hprose/HproseClassManager.phpr../../../../Library/HproseFormatter.php./ThinkPHP/Library/Vendor/Hprose/HproseReader.phpary/HproseReader.ph../ThinkPHP/Library/Vendor/Hprose/HproseWriter.phpr.php');
+require_o../ThinkPHP/Library/Vendor/Hprose/HproseFormatter.phpbrary/HproseWriter.php');
+require_once('HproseFormatter.php');
+
+?>
